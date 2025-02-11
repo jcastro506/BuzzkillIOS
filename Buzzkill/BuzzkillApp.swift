5bb05c3c9bcfb31c0f873cdeffd04bd318dac9d1
Joshua Castro <castrojoshua805@gmail.com>
2025-02-10 20:36:46 -0500

    Initial Commit

---
+++ Buzzkill/BuzzkillApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzkillApp.swift
+//  Buzzkill
+//
+//  Created by Josh Castro on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BuzzkillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuzzkillTests/BuzzkillTests.swift
@@ -0,0 +1,16 @@
+//
+//  BuzzkillTests.swift
+//  BuzzkillTests
+//
+//  Created by Josh Castro on 2/10/25.
+//
+
+import Testing
+
+struct BuzzkillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
